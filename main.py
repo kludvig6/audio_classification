efb017975b87b56708f8e1ade914e8108558cc64
Ludvig <ludvig98@lyse.net>
2023-04-23 14:57:43 +0200

    Updated gitignore to get rid of errors with python venv

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ if __name__ == "__main__":
     
     
 """
-Next to implement: The clusters does not have genres given to them. This should be implemented. The clustering algorithm is per class, but when implementing the nearest neighbour, class will be needed.
-
 Implement k Nearest Neighbour. This should input a vector and a set of clusters, then find the k nearest clusters from the vector, and use this to classify the vector into the correct genre.
 
 Generate confusion matrix and error rate for the test set.
