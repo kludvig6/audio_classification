a13252c3e996799ded889ff9c5de0dbfd9af131d
Ludvig <ludvig98@lyse.net>
2023-04-30 20:05:03 +0200

    Functioning but bad neural network

--- main.py
+++ main.py
@@ -4,20 +4,14 @@ from knn import knn
 from error_rate import error_rate, confusion_matrix, error_rate_percentage, plot_confusion_matrix, specific_error_rates
 from audio_track import AudioTrackTable
 from knn_using_sklearn import knn_sklearn_classifier
-import numpy as np
+from sklearn.preprocessing import StandardScaler
 
-
-
-import os
 import torch
-from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from tqdm import tqdm
 from neural_network import NeuralNetwork, train, test, one_hot_encoder
 
-
-BATCH_SIZE = 100
+BATCH_SIZE = 700
+NUM_FEATURES = 63
 NUMBER_OF_NEIGHBOURS = 5
 TASK_NUMBER = 1
 
@@ -34,11 +28,20 @@ TASK_2_FEATURES = [
     FeatureTranslationTable.spectral_centroid_mean.value,
 ]
 
+TASK_2_FEATURES_TESTS = [
+    FeatureTranslationTable.spectral_rolloff_mean.value,
+    FeatureTranslationTable.mfcc_1_mean.value,
+    FeatureTranslationTable.tempo.value,
+    #FeatureTranslationTable.spectral_centroid_mean.value,
+]
+
 TASK_3_FEATURES = [
     FeatureTranslationTable.spectral_rolloff_mean.value,
     FeatureTranslationTable.mfcc_1_mean.value,
     FeatureTranslationTable.spectral_centroid_mean.value,
 ]
+
+TASK_4_FEATURES = range(NUM_FEATURES)
 
 def neural_network_main():
     device = (
@@ -54,41 +57,81 @@ def neural_network_main():
     training_set = audio_track_table.get_train_set()
     test_set = audio_track_table.get_test_set()
     
-    feature_idxs = range(63)
+    feature_idxs = TASK_4_FEATURES
+    
+    scaler = StandardScaler()
     
     X_train = [song.extract_features(feature_idxs) for song in training_set]
+    X_train = scaler.fit_transform(X_train)
+    
     y_train = [song.genre_id for song in training_set]
     y_train = [one_hot_encoder(y) for y in y_train]
     
     X_test = [song.extract_features(feature_idxs) for song in test_set]
-    y_test = [song.genre_id for song in test_set]
-    #y_test = [one_hot_encoder(y) for y in y_test]
+    X_test = scaler.fit_transform(X_test)
     
-    x_train_loader = DataLoader(X_train, batch_size=BATCH_SIZE, drop_last=True)
-    y_train_loader = DataLoader(y_train, batch_size=BATCH_SIZE, drop_last=True)
+    y_test = [song.genre_id for song in test_set]
+        
+    x_train_loader = DataLoader(X_train, batch_size=len(X_train), drop_last=True)
+    y_train_loader = DataLoader(y_train, batch_size=len(X_train), drop_last=True)
 
     x_test_loader = DataLoader(X_test, batch_size=1, drop_last=True)
     y_test_loader = DataLoader(y_test, batch_size=1, drop_last=True)
     
-    
-    model = NeuralNetwork().to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    print(model)
+    model = NeuralNetwork(len(feature_idxs)).to(device)
+    optimizer = torch.optim.Adagrad(model.parameters(), lr=0.001)
     
     train(model, x_train_loader, y_train_loader, optimizer, device)
     
     classification_results = test(model, x_test_loader, y_test_loader, device)
-
+    for result in classification_results:
+        result[0] = int(result[0])
+        
     err_rate = error_rate(classification_results)
     conf_matrix = confusion_matrix(classification_results)
-    err_rate_per_genre = specific_error_rates(conf_matrix)
     
     print("Error rate:", err_rate)
-    #print("Error rate per genre: ", err_rate_per_genre)
     print("Confusion matrix:\n", conf_matrix)
+
+def main_find_lowest_error_rate():
+    audio_track_table = AudioTrackTable("GenreClassData_30s.txt")
+    genre_lst = GenreList()
     
+    feature_idxs_baseline = TASK_2_FEATURES_TESTS
+    
+    lowest_error_rate = 1
+    lowest_error_rate_idx = 0
+    for i in range(63):
+        if i in feature_idxs_baseline:
+            pass
+        feature_idxs = feature_idxs_baseline + [i]
+        
+        training_set = audio_track_table.get_train_set()
+        test_set = audio_track_table.get_test_set()
+
+        classification_results = []
+
+        for song in test_set:
+            classified_genre = knn(song, training_set, feature_idxs, NUMBER_OF_NEIGHBOURS)
+            classified_genre_id = genre_lst.genres.index(classified_genre)
+            true_genre_id = song.genre_id
+            classification_results.append([true_genre_id, classified_genre_id]) 
+
+        for result in classification_results:
+            result[0] = int(result[0])
+
+        err_rate = error_rate(classification_results)
+        conf_matrix = confusion_matrix(classification_results)
+    
+        print("Error rate:", err_rate)
+        print("Confusion matrix:\n", conf_matrix)
+        
+        if err_rate < lowest_error_rate:
+            lowest_error_rate = err_rate
+            lowest_error_rate_idx = i
+    print(f"Lowest error rate is {lowest_error_rate} for feature {lowest_error_rate_idx}")
+        
 def main():
-    
     knn_sklearn_classifier()
     
     audio_track_table = AudioTrackTable("GenreClassData_30s.txt")
@@ -112,25 +155,18 @@ def main():
         true_genre_id = song.genre_id
         classification_results.append([true_genre_id, classified_genre_id]) 
     
+    for result in classification_results:
+        result[0] = int(result[0])
+        
     err_rate = error_rate(classification_results)
     conf_matrix = confusion_matrix(classification_results)
-    err_rate_per_genre = specific_error_rates(conf_matrix)
     
     print("Error rate:", err_rate)
-    #print("Error rate per genre: ", err_rate_per_genre)
     print("Confusion matrix:\n", conf_matrix)
     
     plot_confusion_matrix(conf_matrix)
     
 if __name__ == "__main__":
+    #main_find_lowest_error_rate()
+    #neural_network_main()
     main()
-    neural_network_main()
-    
-    
-"""
-Generate confusion matrix and error rate for the test set.
-
-Fix the error with covariance and empty sets.
-
-Write a neural network for task 4.
-""" 
--- neural_network.py
+++ neural_network.py
@@ -1,85 +1,85 @@
-import os
-import numpy as np
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 from tqdm import tqdm
 
-"""device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
-print(f"Using {device} device")"""
-
-
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, _num_features):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(63, 50),
+        self.num_features = _num_features
+        self.convolution_stack = nn.Sequential(
+            nn.Conv1d(1, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm1d(32),
             nn.ReLU(),
-            nn.Linear(50, 50),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+        
+            nn.Conv1d(32, 64, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm1d(64),
             nn.ReLU(),
-            nn.Linear(50, 10),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+        
+            nn.Conv1d(64, 128, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+
+        )
+        self.linear_stack = nn.Sequential(        
+            nn.Linear(896, 256),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            
+            nn.Linear(256, 128),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            
+            nn.Linear(128, 64),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            
+            nn.Linear(64, 10),
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        x = x.unsqueeze(1)  # add a channel dimension
+        x = self.convolution_stack(x)
+        x = x.view(x.size(0), -1)
+        logits = self.linear_stack(x)
         pred_probab = nn.Softmax(dim=1)(logits)
         return pred_probab
 
-def train(model, x_train_loader, y_train_loader, optimizer, device, loss_fn = nn.MSELoss()):
-    NUMBER_OF_EPOCHS = 80
-    for epoch in range(NUMBER_OF_EPOCHS):
-        accumulated_loss = 0
-        accumulated_accuracy = 0
+def train(model, x_train_loader, y_train_loader, optimizer, device, loss_fn = nn.CrossEntropyLoss()):
+    NUMBER_OF_EPOCHS = 1000
+    accumulated_loss = 0
+    accumulated_accuracy = 0
+    for _ in range(NUMBER_OF_EPOCHS):
         for (x_train, y_train) in zip(x_train_loader, y_train_loader):
-            
             x_train = x_train.to(device).to(torch.float32)
             y_train = y_train.to(device).to(torch.float32)
-
             model.zero_grad()
+            
             pred_probab = model(x_train)
-            
             loss = loss_fn(pred_probab, y_train)
-            
             accumulated_loss += loss.item()
             accumulated_accuracy += (1-torch.abs(pred_probab - y_train).mean().item())
             loss.backward() 
             optimizer.step()
             
-            #print(f"Average loss epoch {epoch}:", accumulated_loss/x_train.shape[1])
-            #print(f"Average accuracy epoch {epoch}:", accumulated_accuracy/x_train.shape[1])
+    print(f"Average loss:", 100*accumulated_loss/(NUMBER_OF_EPOCHS*x_train.shape[1]))
+    print(f"Average accuracy:", 100*accumulated_accuracy/(NUMBER_OF_EPOCHS*x_train.shape[1]))
             
 def test(model, x_test_loader, y_test_loader, device):
     classification_results = []
     with torch.no_grad():
-        i = 0
         for (x_test, y_test) in tqdm(zip(x_test_loader, y_test_loader),desc="Testing"):
-            
             x_test = x_test.to(device).to(torch.float32)
             y_test = y_test.to(device).to(torch.float32)
             
             pred_probab = model(x_test)
-            
-            #loss = loss_fn.item(pred_probab, y_test)
-            #test_loss = loss.item()
-            
             y_pred = pred_probab.argmax(1)
-            #y_test = y_test.argmax(1)
-            
-            classification_results.append([y_test.item(), y_pred.item()])
-            
-    return classification_results
-            
+            classification_results.append([y_test.item(), y_pred.item()])     
+    return classification_results  
 
 def one_hot_encoder(y):
     encoded_y = torch.zeros(10)
-    encoded_y[int(y)-1] = 1
+    encoded_y[int(y)] = 1
     return encoded_y
